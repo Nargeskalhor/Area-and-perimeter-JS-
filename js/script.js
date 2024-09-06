430d1ec70c46bd1d06c6e09a12c0c4a55fb565af
narges <13.narges.kalhor.82@gmail.com>
2024-09-06 16:27:23 +0330

    add a file js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let length = 2;
+let width = 4;
+// let area = length * width;
+// let perimeter = 2 * (length + width);
+// console.log("rectangle:");
+// console.log("area", area);
+// console.log("length", length);
+
+// let qatar =9 ;
+// let p =3.14;
