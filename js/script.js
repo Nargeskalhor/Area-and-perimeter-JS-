eac60f66bc1fdce5d918a11b1b032b0e97d33ae0
narges <13.narges.kalhor.82@gmail.com>
2024-09-06 16:30:36 +0330

    add rectangle

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 let length = 2;
 let width = 4;
+let area = length * width;
+let perimeter = 2 * (length + width);
+console.log("rectangle:");
+console.log("area", area);
+console.log("length", length);
